e1165a055e7c6122901bf5641884093830f0bc78
OpenJ92 <jacob.vartuli.92@gmail.com>
2025-01-02 01:40:47 -0500

    fix import bug in v0.1.0 and reupload

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt", "r") as f:
 
 setup(
     name="storage-bridge",
-    version="0.1.0",
+    version="0.1.2",
     packages=find_packages(),
     install_requires=requirements,
     author="Jacob Vartuli-Schonberg",
